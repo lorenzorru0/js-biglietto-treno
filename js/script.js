1cd43e3f95311abfd219e55891882b76ecb08e68
lorenzorru0 <lorenzoorru999@gmail.com>
2021-07-30 09:39:24 +0200

    Fix for multiple calc

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 function calcoloPrezzo() {
     // 1. Richiesta distanza tragitto
     var distanzaKm = document.getElementById("distanzaKm").value;
-    document.getElementById('distanzaKmP').innerHTML += distanzaKm;
+    document.getElementById('distanzaKmP').innerHTML = distanzaKm;
     // 2. Richiesta età passegero
     var eta = document.getElementById("eta").value;
-    document.getElementById('etaP').innerHTML += eta;
+    document.getElementById('etaP').innerHTML = eta;
     // 3. Calcolo del prezzo del biglietto 
     var prezzo = distanzaKm * 0.21;
     if (eta < 18) {
         var prezzo = prezzo - (prezzo * 0.2);
-        document.getElementById('sconto').innerHTML += "Sconto del 20% per i minori di 18 anni";
+        document.getElementById('sconto').innerHTML = "Sconto del 20% per i minori di 18 anni";
     } else if (eta > 65) {
         var prezzo = prezzo - (prezzo * 0.4);
-        document.getElementById('sconto').innerHTML += "Sconto del 40% per le persone sopra i 65 anni";
+        document.getElementById('sconto').innerHTML = "Sconto del 40% per le persone sopra i 65 anni";
     } else {
-        document.getElementById('sconto').innerHTML += "Nessuno sconto applicato";
+        document.getElementById('sconto').innerHTML = "Nessuno sconto applicato";
     }
     console.log(prezzo);
     // 4. Stampo all'utente il risultato
-    document.getElementById('prezzo').innerHTML += prezzo.toFixed(2) + "€";
+    document.getElementById('prezzo').innerHTML = prezzo.toFixed(2) + "€";
 }
